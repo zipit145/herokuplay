b618bbc063a8754686995a579e9ed56f42e4ef84
zipit145 <gregbforney@gmail.com>
2018-11-28 14:43:05 -0700

    added some data

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const port = 3000
-
+var data =  [
+    {id: 1, word: "word9"},
+    {id: 2, word: "word10"},
+    {id: 3, word: "word11"}
+]
 app.use(cors())
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/words', (req, res) => {
+    res.send({data})
+})
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
