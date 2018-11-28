1e8fdbae64d72014478a18e66a826f5017e86b75
zipit145 <gregbforney@gmail.com>
2018-11-28 14:33:41 -0700

    updating index.js

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const port = 3000
 
+app.use(cors())
+
 app.get('/', (req, res) => res.send('Hello World!'))
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
