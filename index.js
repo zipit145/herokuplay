31be8572072f80091d4573c9788b38506f0a52ef
zipit145 <gregbforney@gmail.com>
2018-12-03 15:13:25 -0700

    fullcrud deploy to heroku

--- index.js
+++ index.js
@@ -3,15 +3,29 @@ const app = express()
 const cors = require('cors')
 const port = process.env.PORT || 3000
 const queries = require('./queries')
+const bodyparser = require('body-parser')
 var data =  [
     {id: 1, word: "word9"},
     {id: 2, word: "word10"},
     {id: 3, word: "word11"}
 ]
 app.use(cors())
+app.use(bodyparser.json())
 
 app.get('/', (req, res) => {
     queries.listAll().then(students => res.send(students))
+})
+app.get('/:id', (req, res) => {
+    queries.getById(req.params.id).then(students => res.send(students))
+})
+app.post('/', (req, res) => {
+    queries.createStudent(req.body).then(students => res.send(students[0]))
+})
+app.delete('/:id', (req,res) => {
+    queries.deleteStudent(req.params.id).then(res.sendStatus(204))
+})
+app.put('/:id', (req, res) => {
+    queries.updateStudent(req.params.id, req.body).then(updatedStudent => res.json(updatedStudent[0]))
 })
 
 
--- queries.js
+++ queries.js
@@ -3,5 +3,18 @@ const database = require('./database-connection')
 module.exports = {
     listAll(){
         return database('students')
+    },
+    getById(id){
+        return database('students').where('id',id)
+    },
+    createStudent(newStudent) {
+        return database('students').insert(newStudent).returning('*')
+    },
+    deleteStudent(id) {
+        return database('students').where('id',id).delete()
+    },
+    updateStudent(id, updatedStudentInfo) {
+        return database('students').where('id',id).update(updatedStudentInfo).returning('*')
     }
-  }
+}
+
